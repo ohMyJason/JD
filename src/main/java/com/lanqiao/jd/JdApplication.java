a1f66fc46a6b43dfec9e66c2bd974dc467096b9b
ohmyjason <719134834@qq.com>
2019-08-12 17:29:44 +0800

    first commit

---
+++ src/main/java/com/lanqiao/jd/JdApplication.java
@@ -0,0 +1,13 @@
+package com.lanqiao.jd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/lanqiao/jd/service/HelloService.java
@@ -0,0 +1,9 @@
+package com.lanqiao.jd.service;
+
+/**
+ * @author 刘佳昇
+ * @Date 2019/8/12 16:58
+ */
+
+public interface HelloService {
+}
