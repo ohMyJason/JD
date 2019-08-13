f2575d2ef84cd834d40962334bd26d95ec5e6506
ohmyjason <719134834@qq.com>
2019-08-14 00:26:15 +0800

    add userController

--- src/main/java/com/lanqiao/jd/controller/UserController.java
+++ src/main/java/com/lanqiao/jd/controller/UserController.java
@@ -1,14 +1,27 @@
 package com.lanqiao.jd.controller;
 
-import com.lanqiao.jd.util.FileUtil;
+import com.lanqiao.jd.dao.UserMapper;
+import com.lanqiao.jd.entity.User;
+import com.lanqiao.jd.util.Result;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
 
 /**
  * @author 刘佳昇
  * @Date 2019/8/13 14:39
  */
-
+@RestController
 public class UserController {
+    @Autowired
+    UserMapper userMapper;
+
+    @PostMapping("/login")
+    public Result login(User user){
+        return null;
+    }
+
+
+
 }
