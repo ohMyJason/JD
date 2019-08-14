432f4d16d708914ae6d6fe8f2489b42e495ae511
ohmyjason <719134834@qq.com>
2019-08-14 15:44:47 +0800

    加入JWT登录验证 加入登录注解 加入拦截器，拦截所有请求

--- src/main/java/com/lanqiao/jd/controller/UserController.java
+++ src/main/java/com/lanqiao/jd/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.lanqiao.jd.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.lanqiao.jd.annotations.UserLoginToken;
 import com.lanqiao.jd.entity.User;
+import com.lanqiao.jd.service.TokenService;
 import com.lanqiao.jd.service.UserService;
 import com.lanqiao.jd.util.Result;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -14,15 +15,45 @@ public class UserController {
     @Autowired
     UserService userService;
 
+    @Autowired
+    TokenService tokenService;
     //注册功能->向数据库中插入一条user记录
     //need:userName password  phoneNumber
     @PostMapping("/register")
-    public Result register(User user){
+    public Result register(User user) {
         return userService.register(user);
     }
 
+    //    @PostMapping("/login")
+//    public Result login(User user){
+//        return userService.login(user);
+//    }
+
+
     @PostMapping("/login")
-    public Result login(User user){
-        return userService.login(user);
+    public Object login(@RequestBody User user) {
+        JSONObject jsonObject = new JSONObject();
+        User userForBase = userService.findByUsername(user);
+        if (userForBase == null) {
+            jsonObject.put("message", "登录失败,用户不存在");
+            return jsonObject;
+        } else {
+            if (!userForBase.getPassword().equals(user.getPassword())) {
+                jsonObject.put("message", "登录失败,密码错误");
+                return jsonObject;
+            } else {
+                String token = tokenService.getToken(userForBase);
+                jsonObject.put("token", token);
+                jsonObject.put("user", userForBase);
+                return jsonObject;
+            }
+        }
     }
+
+    @UserLoginToken
+    @PostMapping("/getMessage")
+    public String getMessage(){
+        return "你已通过验证";
+    }
+
 }
