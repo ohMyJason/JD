c805787af56d317f2f3ee23dc2439ec0f177112d
C-H-Han <admin@honkerac.cn>
2019-08-15 17:54:33 +0800

    wh 新增地址插入与删除

---
+++ src/main/java/com/lanqiao/jd/service/UserAddressService.java
@@ -0,0 +1,10 @@
+package com.lanqiao.jd.service;
+
+import com.lanqiao.jd.entity.UserAddress;
+import com.lanqiao.jd.util.Result;
+
+public interface UserAddressService {
+    public Result insertUserAddress(UserAddress userAddress);
+
+    public Result deleteUserAddress(int userAddressId);
+}
