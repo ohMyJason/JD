d7d94b7e36b69b4b320da4f61a5b2f76c85f12d4
C-H-Han <admin@honkerac.cn>
2019-08-17 15:35:15 +0800

    wh 新增拦截和主页显示名称

--- src/main/resources/static/user/js/cart.js
+++ src/main/resources/static/user/js/cart.js
@@ -1,6 +1,41 @@
 ($(function () {
-        show();
-
+        $.ajax({
+                url: "http://localhost:8080/user/showCartItem",
+                type: "post",
+                dataType: "json",
+                headers:{'token':$.cookie("token")
+                },
+                data: {
+                    userId: $.cookie('userId')
+                },
+                success: function (result) {
+                    if(result.code == 0) {
+                        var $node = $(".cart-item-list").clone(true);
+                        $(".cart-item-list").detach();
+                        for (var i = 0; i < result.count; i++) {
+                            // var $node = $(".cart-item-list").detach();
+                            $node.children().eq(0).find("a").children().html(result.data[i].businessName);
+                            var imgsrc = ".." + result.data[i].productImgUrl;
+                            var $temp = $node.children().eq(2).children();
+                            $temp.eq(0).attr("src", imgsrc);
+                            $temp.eq(1).html(result.data[i].productName);
+                            $temp.eq(2).html(result.data[i].detail1);
+                            $temp.eq(3).html(result.data[i].productPrice);
+                            $temp.eq(4).children().eq(1).val(result.data[i].num);
+                            $temp.eq(5).html(result.data[i].productId);
+                            $temp.eq(6).html(result.data[i].productPrice * result.data[i].num);
+                            $temp.eq(7).html(result.data[i].cartItemId);
+                            $("#jd-cart").append($node);
+                            $node = $("#jd-cart").children().eq(1).clone(true);
+                        }
+                    }else {
+                        window.location.href="/user/login.html";
+                    }
+                },
+                error: function () {
+                    alert("失败");
+                }
+        })
         //添加数量
         $("#add-btn").click(function () {
             $.ajax({
@@ -147,6 +182,8 @@
                 url: "http://localhost:8080/user/showCartItem",
                 type: "post",
                 dataType: "json",
+                headers:{'token':$.cookie("token")
+                },
                 data: {
                     userId: 3
                 },
