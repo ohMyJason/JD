5b58dfb3876cce9a31c46ddd4b78a08dfc134ca9
C-H-Han <admin@honkerac.cn>
2019-08-17 17:44:34 +0800

    过度页面查询

--- src/main/java/com/lanqiao/jd/dao/OrderMapper.java
+++ src/main/java/com/lanqiao/jd/dao/OrderMapper.java
@@ -1,8 +1,11 @@
 package com.lanqiao.jd.dao;
 
+import com.lanqiao.jd.entity.Cart_Product_Business;
 import com.lanqiao.jd.entity.Order;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Mapper
 @Repository
@@ -18,4 +21,6 @@ public interface OrderMapper {
     int updateByPrimaryKeySelective(Order record);
 
     int updateByPrimaryKey(Order record);
+
+    Cart_Product_Business showItem(Integer cartItemId);
 }
