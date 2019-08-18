bf248fdf79a43fe59bde69e841c485f1f87e28b0
Xionglin <1216612753@qq.com>
2019-08-18 08:14:17 +0800

    order

--- src/main/resources/static/user/js/order.js
+++ src/main/resources/static/user/js/order.js
@@ -1,41 +1,46 @@
 ($(function () {
-    var str = [1,3,4,5,6];
-    $.ajax({
-        url:"/user/showItem",
-        type: "post",
-        dataType: "json",
-        // headers:{'token':$.cookie("token")},
-        data: {
-            IdArry:str
-            // userId: $.cookie('userId')
-        },
-        success:function (result) {
-            if(result.code == 0){
-                alert(1);
-                // var $node = $("#dcr_cont").clone(true);
-                // $("#dcr_cont").detach();
-                // for(var i = 0; i < result.count; i++){
-                //     $node.prev().html(result.data[i].businessName);
-                //     var imgsrc = ".." + result.data[i].productImgUrl;
-                //     $node.eq(0).children().attr("src",imgsrc);
-                //     $node.eq(1).children().eq(0).eq(0).html(result.data[i].productName);
-                //     // $node.eq(1).children().eq(0).eq(1).html(result.data[i].);
-                //     $node.eq(1).eq(1).eq(0).html(result.data[i].productPrice);
-                //     $node.eq(1).eq(2).children().html(result.data[i].num);
-                //     $("#deliver_cont_right").append($node);
-                //     $node = $("#dcr_cont").clone(true);
-                // }
-            }else {
-                alert("error");
-                // window.location.href="/user/login.html";
-            }
-        },
-        error: function () {
-            alert("失败");
+
+    show();
+        function show() {
+            $.ajax({
+                url: "http://localhost:8080/user/showItem",
+                type: "post",
+                traditional:true,
+                dataType: "json",
+                data: {
+                    IdArry: ['1','3','4','5','6']
+                },
+                success: function (result) {
+                    if(result.code == 0){
+                        alert("success");
+                        // alert(result.data[0]);
+                        // alert(result.data[0].businessName);  //商家2
+                        // alert(result.data[1].productId);     //6
+                        // alert(result.data[1].productName);   //苹果
+                        // alert(result.data[1].producePrice);  //20
+                        // alert(result.data[1].productImgUrl); //图片
+                        // alert(result.data[1].detail1);       //详情null
+                        // alert(result.data[1].num);           //数量
+                        // alert(result.data[1].cartItemId);    //4
+                        var $node = $("#change_box").clone(true);
+                        // $("#change_box").detach();
+                        for(var i=0; i < 1; i++){
+                            $node.children().eq(0).html("店名");
+                            var imgsrc = "../img/apple.png";
+                            $node.children().eq(1).find("img").attr("src",imgsrc);
+                            $node.children().eq(1).find("#dcr_t_left").children("ul").children("li").eq(0).html("苹果");
+                            $node.children().eq(1).find("#dcr_t_mid").children().eq(0).html("20元");
+                            $node.children().eq(1).find("#dcr_t_num").children().html("x3");
+                            $("#deliver_cont_right").append($node);
+                            $node = $("#change_box").clone(true);
+                        }
+                    }else{
+                        alert("失败");
+                    }
+                },error: function () {
+                    alert("error");
+                }
+            })
         }
     })
-
-        //几件商品
-        // $("#productNum").html();
-})
 )
